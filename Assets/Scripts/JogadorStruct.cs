2af95a2d8349ea4ab313625ea800644dc54b6c2d
Daniel Mattos <daniel_mattos@id.uff.br>
2021-09-07 21:25:45 -0300

    Jogador Struct

--- Assets/Scripts/JogadorStruct.cs
+++ Assets/Scripts/JogadorStruct.cs
@@ -1,14 +1,17 @@
 using Firebase.Firestore;
 
-[FirestoreData]
-public struct JogadorStruct
+namespace Trunfo
 {
-    [FirestoreProperty]
-    public string Name { get; set; }
+    [FirestoreData]
+    public struct JogadorStruct
+    {
+        [FirestoreProperty]
+        public string Name { get; set; }
 
-    [FirestoreProperty]
-    public string UserToken { get; set; }
+        [FirestoreProperty]
+        public string UserToken { get; set; }
 
-    [FirestoreProperty]
-    public bool Status { get; set; }
+        [FirestoreProperty]
+        public bool Status { get; set; }
+    }
 }
