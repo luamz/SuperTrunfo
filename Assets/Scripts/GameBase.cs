3645735dc228ee24c0fbf5182f1dc0bad1dc1657
Daniel Mattos <daniel_mattos@id.uff.br>
2021-09-08 00:16:00 -0300

    Jogador status

---
+++ Assets/Scripts/GameBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Firebase.Auth;
+using UnityEngine;
+using Firebase.Firestore;
+
+namespace Trunfo
+{
+    public class GameBase : AuthManager
+    {
+        void OnApplicationQuit()
+        {
+            Debug.Log("Saindo");
+            Debug.Log(this.jogadorData.Name);
+            if (this.jogadorData.UserToken != null)
+            {
+                this.status = false;
+                UpdateFirestore(this.jogadorData.UserToken);
+            }
+
+        }
+    }
+}
