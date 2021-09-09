907aaa5f539836a9034df817797206842997982a
Luam <luamsilva@id.uff.br>
2021-09-09 04:02:13 -0300

    Notificacoes e entrar em partida

--- Assets/Scripts/EnviaNotificacao.cs
+++ Assets/Scripts/EnviaNotificacao.cs
@@ -11,12 +11,14 @@ namespace Trunfo
     {
         public void Envia()
         {
+            string sala_id="aaaa";
 
             var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
 
             request.KeepAlive = true;
             request.Method = "POST";
             request.ContentType = "application/json; charset=utf-8";
+            
 
             request.Headers.Add("authorization", "Basic YzE4NTU5MjItOGZiMy00MTQ0LTgwZTUtOGZlNTYxMTY2NzUw");
 
@@ -27,9 +29,10 @@ namespace Trunfo
 
             byte[] byteArray = Encoding.UTF8.GetBytes("{"
                                             + "\"app_id\": \"96f64d30-af66-4072-acbb-a98c3d4e9763\","
-                                            + "\"contents\": {\"en\": \"Cadê vocês galerinha? \"},"
+                                            + "\"contents\": {\"en\": \"FUNCIONOU\"},"
+                                            + "{\"sala_id\": \""+sala_id+"\"},"
                                             + "\"filters\": [{\"field\": \"last_session\", \"key\": \"session_time\", \"relation\": \">\", \"value\": \"30.0\"}]}");
-
+                                        
             string responseContent = null;
 
             try
--- Assets/Scripts/Notifications.cs
+++ Assets/Scripts/Notifications.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace Trunfo
 {
     public class Notifications : MonoBehaviour
     {
+
         // Start is called before the first frame update
         void Start()
         {
             // Uncomment this method to enable OneSignal Debugging log output 
             // OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-            
+
             // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
             OneSignal.StartInit("96f64d30-af66-4072-acbb-a98c3d4e9763")
                 .HandleNotificationOpened(OneSignalHandleNotificationOpened)
@@ -19,29 +22,38 @@ namespace Trunfo
                 { OneSignal.kOSSettingsAutoPrompt, false },
                 { OneSignal.kOSSettingsInAppLaunchURL, false } })
                 .EndInit();
-            
+
             OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
-            
+
             // iOS - Shows the iOS native notification permission prompt.
             //   - Instead we recomemnd using an In-App Message to prompt for notification 
             //     permission to explain how notifications are helpful to your users.
             OneSignal.PromptForPushNotificationsWithUserResponse(OneSignalPromptForPushNotificationsReponse);
+        }
+
+        // Gets called when the player opens a OneSignal notification.
+        private static void OneSignalHandleNotificationOpened(OSNotificationOpenedResult result)
+        {
+            string sala_id = result[sala_id];
+            if (sala_id == "aaaa")
+            {
+                SceneManager.LoadScene("Mesa");
             }
 
-            // Gets called when the player opens a OneSignal notification.
-            private static void OneSignalHandleNotificationOpened(OSNotificationOpenedResult result) {
+            SceneManager.LoadScene("EntrarPartida");
             // Place your app specific notification opened logic here.
-            }
+        }
 
-            // iOS - Fires when the user anwser the notification permission prompt.
-            private void OneSignalPromptForPushNotificationsReponse(bool accepted) {
+        // iOS - Fires when the user anwser the notification permission prompt.
+        private void OneSignalPromptForPushNotificationsReponse(bool accepted)
+        {
             // Optional callback if you need to know when the user accepts or declines notification permissions.
-}
+        }
 
         // Update is called once per frame
         void Update()
         {
-        
+
         }
     }
 }
