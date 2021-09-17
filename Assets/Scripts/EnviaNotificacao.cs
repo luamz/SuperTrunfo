f9023439fd446dadfe23680d33d0976015428843
Luam <luamsilva@id.uff.br>
2021-09-17 13:05:43 -0300

    Reconfigura notificações

---
+++ Assets/Scripts/EnviaNotificacao.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Net;
+using System.Text;
+
+//Esse modulo envia notificacao para o usuario
+namespace Trunfo
+{
+    public class EnviaNotificacao : MonoBehaviour
+    {
+        public static void Envia(string idSala)
+        {
+
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+            
+
+            request.Headers.Add("authorization", "Basic REST API KEY");
+
+            byte[] byteArray = Encoding.UTF8.GetBytes("{"
+                                            + "\"app_id\": \"APP KEY\","
+                                            + "\"headings\": {\"en\": \"Venha jogar!!\"},"
+                                            + "\"contents\": {\"en\": \"Alguém te convidou para jogar Super Trunfo\"},"
+                                            + "\"data\": {\"sala\": \""+idSala+"\"},"
+                                            + "\"filters\": [{\"field\": \"last_session\", \"key\": \"session_time\", \"relation\": \">\", \"value\": \"30.0\"}]}");
+                                        
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+                Debug.Log(responseContent);
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+
+            System.Diagnostics.Debug.WriteLine(responseContent);
+        }
+    }
+}
---
+++ Assets/Scripts/Notifications.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+/* 
+Essa classe é responsável por adicionar e configurar o OneSignal no App
+*/
+
+namespace Trunfo
+{
+    public class Notifications : MonoBehaviour
+    {
+        private static string salaId = "";
+        public static Action<string> Redireciona;
+
+        void Start()
+        {
+            // Torna um aplicativo notificável pelo One Signal
+            OneSignal.StartInit("APP KEY")
+                .HandleNotificationOpened(OneSignalHandleNotificationOpened)
+                .Settings(new Dictionary<string, bool>() {
+                { OneSignal.kOSSettingsAutoPrompt, false },
+                { OneSignal.kOSSettingsInAppLaunchURL, false } })
+                .EndInit();
+
+            OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+            OneSignal.PromptForPushNotificationsWithUserResponse(OneSignalPromptForPushNotificationsReponse);
+        }
+
+        // Método chamado quando um jogador clica numa notificação
+        public static void OneSignalHandleNotificationOpened(OSNotificationOpenedResult result)
+        {
+            try
+            {
+                // Pega o código da sala que chega pela notificação
+                string cod_sala = result.notification.payload.additionalData["sala"].ToString();
+
+                if (cod_sala != "")
+                {
+                    salaId = cod_sala;
+
+                    // Abre a cena Entrar em Partida
+                    SceneManager.sceneLoaded += SetaIdSala;
+                    SceneManager.LoadScene("EntrarPartida");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+            }
+        }
+
+        private static void SetaIdSala(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "EntrarPartida")
+            {
+               Redireciona?.Invoke(salaId); 
+            }
+        }
+
+        // iOS - É iniciada quando o usuario responde o prompt de permissão de notificação
+        private void OneSignalPromptForPushNotificationsReponse(bool accepted)
+        {
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+
